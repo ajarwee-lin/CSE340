93308c8b54b5712701772f0bf47ff833feb667aa
A. Lincoln Jarwee <122073281+ajarwee-lin@users.noreply.github.com>
2024-05-31 01:11:47 -0600

    commit

--- database/database.js
+++ database/database.js
@@ -2,10 +2,10 @@ const { Pool } = require('pg');
 
 // Create a new pool instance
 const pool = new Pool({
-  user: 'your_database_user',
-  host: 'your_database_host',
-  database: 'your_database_name',
-  password: 'your_database_password',
+  user: 'dbnamecse340', // Updated with the actual database username
+  host: 'dpg-cospjtacn0vc73eofd0g-a', // Updated with the actual database hostname
+  database: 'dbnamecse340', // Update this with your actual database name
+  password: 'Ayp9XzEdFdnEkJy2nI3I6ymyjnwLbbPG', // Update this with your actual database password
   port: 5432, // Change if your database is running on a different port
 });
 
